9a7b47f377870ca187355d38d20241e84bcbb1b3
kai <kaijambro@gmail.com>
2025-05-25 11:29:50 +0800

    Final changes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const one = document.getElementById("one").addEventListener("click", changeTextOne);
-const two = document.getElementById("two").addEventListener("click", changeTextTwo);
-const three = document.getElementById("three").addEventListener("click", changeTextThree);
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+
+one.addEventListener("click", changeTextOne);
+two.addEventListener("click", changeTextTwo);
+three.addEventListener("click", changeTextThree);
 
 let mtext = document.getElementById("main-text");
 let stext = document.getElementById("sub-text");
@@ -8,14 +12,26 @@ let stext = document.getElementById("sub-text");
 function changeTextOne() {
     mtext.innerHTML = "First tab text.";
     stext.innerHTML = "Lorem";
+    
+    one.classList.add("current");
+    two.classList.remove("current");
+    three.classList.remove("current");
 }
 
 function changeTextTwo() {
     mtext.innerHTML = "Second tab text.";
     stext.innerHTML = "Ipsum";
+
+    one.classList.remove("current");
+    two.classList.add("current");
+    three.classList.remove("current");
 }
 
 function changeTextThree() {
     mtext.innerHTML = "Third tab text.";
     stext.innerHTML = "Dolor";
+
+    one.classList.remove("current");
+    two.classList.remove("current");
+    three.classList.add("current");
 }
